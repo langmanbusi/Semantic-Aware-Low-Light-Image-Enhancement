a2a0c6fb41f261f033926b2b474977d8e6ccb98e
langmanbusi <89068081+langmanbusi@users.noreply.github.com>
2023-03-19 22:08:40 +0800

    Delete LLFlow directory

--- LLFlow/code/hrseg/hrseg_lib/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Ke Sun (sunk@mail.ustc.edu.cn)
-# ------------------------------------------------------------------------------
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
